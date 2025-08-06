7be13a4df7255e610c1aa509ae05a4fa51e556c1
sriharinewuser <sri948653@gmail.com>
2025-08-06 11:02:49 +0100

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
 import { fileURLToPath } from 'url';
 import express from 'express';
 import cors from 'cors';
@@ -9,15 +8,12 @@ import userRouter from './routers/user.router.js';
 import orderRouter from './routers/order.router.js';
 // import uploadRouter from './routers/upload.router.js';
 
-
 import { dbconnect } from './config/database.config.js';
-import path,{ dirname } from 'path'
+import path, { dirname } from 'path';
 dbconnect();
-
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
-
 
 const app = express();
 app.use(express.json());
@@ -31,7 +27,7 @@ app.use(
 app.use('/api/foods', foodRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
-//
+// app.use('/api/upload', uploadRouter);
 
 const publicFolder = path.join(__dirname, 'public');
 app.use(express.static(publicFolder));
@@ -41,10 +37,7 @@ app.get('*', (req, res) => {
   res.sendFile(indexFilePath);
 });
 
-
-
-
-const PORT = process.env.port ||5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log('listening on port ' + PORT);
 });
