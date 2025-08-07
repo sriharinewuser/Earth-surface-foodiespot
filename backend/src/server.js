638b0f66a1ff163546d05bb57242e1f89140694d
sriharinewuser <sri948653@gmail.com>
2025-08-07 06:24:24 +0100

    update

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ app.use(express.json());
 app.use(
   cors({
     credentials: true,
-    origin: ['http://localhost:3000','https://earth-surface-foodiespot.vercel.app/'],
+    origin: ['http://localhost:3000','https://earth-surface-foodiespot.vercel.app/','https://foodiespicy00.onrender.com'],
   })
 );
 
