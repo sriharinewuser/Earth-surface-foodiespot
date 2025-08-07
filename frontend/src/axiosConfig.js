cd4a9ae926a9e3e94c2e17cefa676153418f86b7
sriharinewuser <sri948653@gmail.com>
2025-08-07 08:24:26 +0100

    update

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,10 +1,7 @@
-// src/api.js
+
+
+
 import axios from 'axios';
 
-const api = axios.create({
-  baseURL: process.env.NODE_ENV !== 'production'
-    ? 'http://localhost:5000'
-    : 'https://foodiespicy00.onrender.com/',
-})
-
-export default api;
+axios.defaults.baseURL =
+  process.env.NODE_ENV !== 'production' ? 'http://localhost:5000' : '/';
