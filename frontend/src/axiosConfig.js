6e54f183a9bb92d48aa02e8c6cfc753b7e87b27b
sriharinewuser <sri948653@gmail.com>
2025-08-08 09:29:52 +0100

    update

--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 
 axios.defaults.baseURL =
   process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : 'https://foodiespicy00.onrender.com/api';
+
+// Enable credentials (cookies) for cross-origin requests
+axios.defaults.withCredentials = true;
